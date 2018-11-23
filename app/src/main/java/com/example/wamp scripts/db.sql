ad94b13e11be5dc047fbdbcb0ce51e05e34ae493
Dhiraj <dhirajwani70@gmail.com>
2018-11-23 00:38:34 -0500

    updated columns

--- app/src/main/java/com/example/wamp scripts/db.sql
+++ app/src/main/java/com/example/wamp scripts/db.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS aide;
 
-create table users(id integer AUTO_INCREMENT primary key,name text not null,email text not null,phone integer not null, pin integer,gender integer, occupation integer, latitude double, longitude double,type integer);
+create table users(id integer AUTO_INCREMENT primary key,first_name text not null,last_name text not null,email varchar(50) not null,phone integer not null,pin integer,gender integer,occupation integer,latitude double,longitude double,type integer);
