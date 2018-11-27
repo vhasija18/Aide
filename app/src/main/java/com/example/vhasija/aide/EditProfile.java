63882858e00aeb3edddd6fb6119481d3f10c7429
Vhasija <hasijavishwas@gmail.com>
2018-11-27 15:42:42 -0500

    New pages

--- app/src/main/java/com/example/vhasija/aide/EditProfile.java
+++ app/src/main/java/com/example/vhasija/aide/EditProfile.java
@@ -11,9 +11,8 @@ import android.widget.Spinner;
 public class EditProfile extends AppCompatActivity {
 
     EditText first_name,last_name,email,phone,dob;
-    Spinner help;
     String firstname_value,lastname_value,email_value,dob_value;
-    Integer id_value,help_value;
+    Integer id_value;
     Long phone_Value;
     String help_input[]={"Select a service","Ambulance","Doctor","Plumber","Electrician","Mechanic"};
 
@@ -27,23 +26,19 @@ public class EditProfile extends AppCompatActivity {
         email      = findViewById(R.id.editprofile_email);
         phone      = findViewById(R.id.editprofile_phone);
         dob        = findViewById(R.id.editprofile_dob);
-        help       = findViewById(R.id.editprofile_help);
         Intent intent = getIntent();
         firstname_value  = intent.getExtras().getString("firstname");
         lastname_value   = intent.getExtras().getString("lastname");
         email_value      = intent.getExtras().getString("email");
         dob_value        = intent.getExtras().getString("dob");
         id_value         = intent.getExtras().getInt("id");
-        help_value       = intent.getExtras().getInt("occupation");
         phone_Value      = intent.getExtras().getLong("phone");
         first_name.setText(firstname_value);
         last_name.setText(lastname_value);
         email.setText(email_value);
         dob.setText(dob_value);
         phone.setText(phone_Value.toString());
-        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,help_input);
-        help.setAdapter(adapter);
-        help.setSelection(help_value);
+
 
 
 
